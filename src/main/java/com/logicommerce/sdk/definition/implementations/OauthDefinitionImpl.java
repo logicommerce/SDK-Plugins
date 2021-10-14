7101da2a3c48669fd412bcfa0a3fee25c6f59a8e
Francis Castillo <francis.castillo@logicommerce.com>
2021-10-14 11:03:31 +0200

    Change oauthConnect -> oauth

---
+++ src/main/java/com/logicommerce/sdk/definition/implementations/OauthDefinitionImpl.java
@@ -0,0 +1,23 @@
+package com.logicommerce.sdk.definition.implementations;
+
+import com.logicommerce.sdk.definition.OauthDefinition;
+
+public class OauthDefinitionImpl extends ConnectorDefinitionImpl implements OauthDefinition {
+
+	public static class Builder extends ConnectorDefinitionImpl.Builder<Builder, OauthDefinition, OauthDefinitionImpl> {
+
+		@Override
+		protected Builder returnThis() {
+			return this;
+		}
+
+		@Override
+		public OauthDefinition build() {
+			OauthDefinitionImpl connector = new OauthDefinitionImpl();
+			setAttributes(connector);
+			return connector;
+		}
+
+	}
+
+}
