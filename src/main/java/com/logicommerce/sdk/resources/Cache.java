92151a824775406d470ff2059cbd2234c66b21a1
Francesc Requesens <francesc.requesens@logicommerce.com>
2022-12-15 08:11:08 +0100

    Added cache resource

---
+++ src/main/java/com/logicommerce/sdk/resources/Cache.java
@@ -0,0 +1,30 @@
+package com.logicommerce.sdk.resources;
+
+/**
+ * Cache Resource
+ *
+ * @author Logicommerce
+ */
+public interface Cache {
+
+	/**
+	 * Get cached value
+	 *
+	 * @param key Cache key
+	 * @return String
+	 */
+	String get(String key);
+
+	/**
+	 * Put value to cache
+	 * @param key Cache key
+	 * @param value Cache value object
+	 */
+	void put(String key, String value);
+
+	/**
+	 * Purge cache
+	 */
+	void purge();
+
+}
---
+++ src/main/java/com/logicommerce/sdk/resources/CacheSettings.java
@@ -0,0 +1,31 @@
+package com.logicommerce.sdk.resources;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Cache settings
+ *
+ * @author Logicommerce
+ *
+ */
+@Retention(RUNTIME)
+@Target(FIELD)
+public @interface CacheSettings {
+
+	/**
+	 * Name of cache. If it is not defined or empty, the name of cache will be the name of field.
+	 *
+	 * @return a {@link java.lang.String} object
+	 */
+	public String name() default "";
+
+	/**
+	 * Duration of cache in seconds.
+	 * @return a long
+	 */
+	public long duration() default 300;
+
+}
