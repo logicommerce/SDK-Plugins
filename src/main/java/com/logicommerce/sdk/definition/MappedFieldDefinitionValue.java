e9bb14cf73a3780c8848aa51f63cbbb3ed4ed566
Francis castillo <francis.castillo@logicommerce.com>
2025-01-16 15:09:55 +0100

    feat: LC-7113 - mappedFields name-value v2.3.0

---
+++ src/main/java/com/logicommerce/sdk/definition/MappedFieldDefinitionValue.java
@@ -0,0 +1,11 @@
+package com.logicommerce.sdk.definition;
+
+/**
+ * <p>MappedFieldDefinitionValue interface.</p>
+ *
+ * @author LogiCommerce
+ * @since 2.3.0
+ */
+public interface MappedFieldDefinitionValue extends DefinitionValue {
+
+}
--- src/main/java/com/logicommerce/sdk/definition/implementations/PropertyDefinitionValueImpl.java
+++ src/main/java/com/logicommerce/sdk/definition/implementations/PropertyDefinitionValueImpl.java
@@ -1,70 +1,27 @@
 package com.logicommerce.sdk.definition.implementations;
 
-import com.logicommerce.sdk.definition.DefinitionLanguages;
 import com.logicommerce.sdk.definition.PropertyDefinitionValue;
 
 /**
  * <p>PropertyDefinitionValueImpl class.</p>
  *
- * @author Logicommerce
- * @since 1.0.16
+ * @author LogiCommerce
+ * @since 2.3.0
  */
-public class PropertyDefinitionValueImpl implements PropertyDefinitionValue {
+public class PropertyDefinitionValueImpl extends DefinitionValueImpl implements PropertyDefinitionValue {
 
-	private String value;
-	private DefinitionLanguages languages;
-
-	/** {@inheritDoc} */
-	@Override
-	public String getValue() {
-		return value;
-	}
-
-	/** {@inheritDoc} */
-	@Override
-	public DefinitionLanguages getLanguages() {
-		return languages;
-	}
-
-	void setValue(String value) {
-		this.value = value;
-	}
-
-	void setLanguages(DefinitionLanguages languages) {
-		this.languages = languages;
-	}
-
-	public static class Builder<T> {
-
-		private T parentBuilder;
-		private String value;
-		private DefinitionLanguagesImpl.Builder<Builder<T>> languages;
+	public static class Builder<T> extends DefinitionValueImpl.Builder<T, PropertyDefinitionValue, PropertyDefinitionValueImpl> {
 
 		public Builder(T parentBuilder) {
-			this.parentBuilder = parentBuilder;
+			super(parentBuilder);
 		}
 
-		public Builder<T> value(String value) {
-			this.value = value;
-			return this;
+		@Override
+		public PropertyDefinitionValue build() {
+			PropertyDefinitionValueImpl definitionValue = new PropertyDefinitionValueImpl();
+			setAttributes(definitionValue);
+			return definitionValue;
 		}
 
-		public DefinitionLanguagesImpl.Builder<Builder<T>> languages() {
-			this.languages = new DefinitionLanguagesImpl.Builder<>(this);
-			return languages;
-		}
-
-		PropertyDefinitionValue build() {
-			PropertyDefinitionValueImpl properyDefinitionValue = new PropertyDefinitionValueImpl();
-			properyDefinitionValue.setValue(value);
-			if (languages != null) {
-				properyDefinitionValue.setLanguages(languages.build());
-			}
-			return properyDefinitionValue;
-		}
-
-		public T done() {
-			return parentBuilder;
-		}
 	}
 }
